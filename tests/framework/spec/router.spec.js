6dec99ae7cbc7f49b409560fdc91cbbb58d45599
Rollen Gomes <avelliance@gmail.com>
2012-01-31 14:27:14 +0800

    remove unecessary framework folder in tests

--- tests/framework/spec/router.spec.js
+++ tests/framework/spec/router.spec.js
@@ -14,3 +14,4 @@ describe('Router', function(){
     });
   });
 });
+
--- watchr.rb
+++ watchr.rb
@@ -1,4 +1,10 @@
 #watch the spec directories for changes and run tests
 watch( 'tests/framework/spec' ) do
+  puts "Running 'tests/framework/spec'"
   system('jasmine-node tests/framework/')
+end
+
+watch( 'tests/app/features' ) do
+  puts "tests/app/features"
+  system('jasmine-node tests/app/features')
 end
