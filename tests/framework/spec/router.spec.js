a01599c67ce60ad6f5a0554185d398cad8d3861e
Rollen Gomes <avelliance@gmail.com>
2012-01-31 22:12:29 +0800

    add ability to router to default to a error controller on resource not found

--- tests/framework/spec/router.spec.js
+++ tests/framework/spec/router.spec.js
@@ -8,9 +8,17 @@ describe('Router', function(){
 
   describe('route', function(){
     it('should execute a controller action based on exact match', function(){
-      var router = new Router({'/home': controller.index});
+      var router = new Router({'/home': controller.index}, null);
       router.route('/home');
       expect(controller.index).toHaveBeenCalled();
+    });
+
+    it('should default to the errors controller when no appropriate resource is found', function(){
+      errorsController = new Controller();
+      spyOn(errorsController, "index");
+      var router = new Router({}, errorsController);
+      router.route('/fakeroute');
+      expect(errorsController.index).toHaveBeenCalled();
     });
   });
 });
