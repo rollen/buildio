4e69bb1c930c4605749bef6ed4e60ac167877ddf
Rollen Gomes <avelliance@gmail.com>
2012-02-02 17:54:39 +0800

    user can depend on framework router to see angular tests when the go to /tests

--- app/app.js
+++ app/app.js
@@ -8,11 +8,4 @@ Application = function(request, response){
 }
 
 Application.prototype.create = function(){
-  tests_controller = new TestsController(this.request, this.response);
-  errors_controller = new ErrorsController(this.request, this.response);
-
-  controller_map = {};
-  controller_map['/tests'] = tests_controller.index;
-  router = new Router(controller_map, errors_controller);
-  router.route(request.url); 
 }
--- watchr.rb
+++ watchr.rb
@@ -1,14 +1,10 @@
 watch( '(lib/*)') do
-  puts "Running lib features"
-  system('jasmine-node lib/tests/features')
-  puts "Running lib specs"
-  system('jasmine-node lib/tests/spec')
-  puts "Running cotroller tests"
-  system('jasmine-node lib/tests/spc/contollers')
+  puts "\tRunning lib specs"
+  system('jasmine-node ./lib/tests/')
 end
 
 watch( '(app/*)') do
   puts "Running app features"
-  system('jasmine-node app/tests/spec')
+  system('jasmine-node ./app/tests/spec')
 end
 
