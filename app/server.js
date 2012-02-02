7df67d19e2e22b335e1bcbd747e0f37fda3e7cbf
Rollen Gomes <avelliance@gmail.com>
2012-02-02 20:32:12 +0800

    add error handling to framework to respond to requests with no routing options with a 404

---
+++ app/server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+require('./../lib/nervercenter');
+
+http.createServer(function onRequestReceived(request, response){
+  console.log('Routing ' + request.url);
+  filesystem = require('fs');
+  testsController = new TestsController(request, response, filesystem, frameworkViewsPath + '/runner.html');
+  httpVerb = 'GET';
+  pattern = '/tests';
+  message = 'index';
+  route = new HttpRoute(httpVerb, pattern, testsController, message);
+  errorsController = new ErrorsController(request, response);
+  frameworkRouter = new Router([route]);
+  applicationRouter = new Router([]);
+  application = new Application([applicationRouter, frameworkRouter], errorsController, request.url);
+  application.executeRequest();
+}).listen(8888);
