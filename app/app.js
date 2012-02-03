4a27cf1abd796d54f45687093bc53be31141c087
Rollen Gomes <avelliance@gmail.com>
2012-02-03 19:45:51 +0800

    edit router spec file to make variables reference this as receiver

--- app/app.js
+++ app/app.js
@@ -1,11 +1,2 @@
-console.log("Starting application");
-
 require("./../lib/build.js");
 
-Application = function(request, response){
-  this.request = request;
-  this.response = response;
-}
-
-Application.prototype.create = function(){
-}
--- watchr.rb
+++ watchr.rb
@@ -1,10 +1,8 @@
-watch( '(lib/*)') do
+watch( '(app|lib)') do
+  puts "Running app features"
+  system('jasmine-node ./app/tests/spec')
   puts "\tRunning lib specs"
   system('jasmine-node ./lib/tests/')
 end
 
-watch( '(app/*)') do
-  puts "Running app features"
-  system('jasmine-node ./app/tests/spec')
-end
 
