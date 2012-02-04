bc9d2ae7fb643124715e7478ebfbf808ddb56193
Rollen Gomes <avelliance@gmail.com>
2012-02-04 18:40:07 +0800

    fix failing application feature that checks for incorrect routes

--- app/server.js
+++ app/server.js
@@ -1,18 +1,7 @@
 var http = require('http');
 require('./app.js');
 
-
 http.createServer(function onRequestReceived(request, response){
-  console.log('Routing ' + request.url);
-  filesystem = require('fs');
-  testsController = new TestsController(request, response, filesystem, frameworkViewsPath + '/runner.html');
-  httpVerb = 'GET';
-  pattern = '/tests';
-  message = 'index';
-  route = new HttpRoute(httpVerb, pattern, testsController, message);
-  errorsController = new ErrorsController(request, response);
-  frameworkRouter = new Router([route]);
-  applicationRouter = new Router([]);
-  application = new Application([applicationRouter, frameworkRouter], errorsController, request.url);
-  application.executeRequest();
+  console.log(request.url);
+  Nervebuilder.createApplication(request, response, require('fs')).executeRequest();
 }).listen(8888);
