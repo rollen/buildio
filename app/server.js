20faed33147dafa2c7bb22638dc79bc479a7e65f
Rollen Gomes <avelliance@gmail.com>
2012-02-09 01:17:23 +0800

    add name version and empty dependencies hash to package.json to help heroku detect it as a node app

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 require('./app.js');
 
+var port = process.env.PORT || 8888;
 http.createServer(function onRequestReceived(request, response){
   AppBuilder.createApplication(request, response, require('fs')).executeRequest();
-}).listen(8888);
+}).listen(port);
