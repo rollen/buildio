599348b1c236ab2d5116edd9be3204f6be329540
Rollen Gomes <avelliance@gmail.com>
2012-02-29 10:52:08 +0800

    fixed multiple test scenario

--- Procfile2
+++ Procfile2
@@ -1,3 +1,2 @@
 web: node app/server.js
 dev: watchr development/watchr.rb
-testserver: rake -f ./development/Rakefile test:server
--- development/watchr.rb
+++ development/watchr.rb
@@ -1,10 +1,10 @@
 watch( '(app|lib)') do
-  puts "Running app client features"
-  system('rake -f ./development/Rakefile test:run')
   puts "Running app server features"
   system('jasmine-node ./app/tests/')
   puts "\tRunning lib specs"
   system('jasmine-node ./lib/tests/')
+  puts "Running app client features"
+  system('rake -f ./development/Rakefile test:run')
 end
 
 
